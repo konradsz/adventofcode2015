a1e89fc6c8c94e354cd8037acedd1ff4aaf36de4
Konrad <szymoniak.konrad@gmail.com>
2020-02-23 12:31:20 +0100

    add solution to puzzles from day10

---
+++ day10/src/main.rs
@@ -0,0 +1,43 @@
+use itertools::Itertools;
+
+fn process(vec: &Vec<usize>) -> Vec<usize> {
+    vec.iter()
+        .group_by(|&x| x)
+        .into_iter()
+        .fold(Vec::new(), |mut output, (key, group)| {
+            output.push(group.count());
+            output.push(*key);
+            output
+        })
+}
+
+fn part_1() -> usize {
+    let mut vec = vec![1, 3, 2, 1, 1, 3, 1, 1, 1, 2];
+    (0..40).for_each(|_| vec = process(&vec));
+    vec.len()
+}
+
+fn part_2() -> usize {
+    let mut vec = vec![1, 3, 2, 1, 1, 3, 1, 1, 1, 2];
+    (0..50).for_each(|_| vec = process(&vec));
+    vec.len()
+}
+
+fn main() {
+    assert_eq!(492982, part_1());
+    assert_eq!(6989950, part_2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        assert_eq!(process(&vec![1]), vec![1, 1]);
+        assert_eq!(process(&vec![1, 1]), vec![2, 1]);
+        assert_eq!(process(&vec![2, 1]), vec![1, 2, 1, 1]);
+        assert_eq!(process(&vec![1, 2, 1, 1]), vec![1, 1, 1, 2, 2, 1]);
+        assert_eq!(process(&vec![1, 1, 1, 2, 2, 1]), vec![3, 1, 2, 2, 1, 1]);
+    }
+}
